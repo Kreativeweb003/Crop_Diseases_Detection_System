b33fc446247881fbe85a7c4b799e97785723ab54
koreeb (system) <koreeb@gmail.com>
2026-09-18 13:31:47 +0100

    ML integrated

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="upload/", permanent=False)),
     path("", include("detection.urls")),
     path("diseases/", include("diseases.urls")),
     path("accounts/", include("accounts.urls")),
